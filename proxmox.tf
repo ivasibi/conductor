56b2537e24587586a9df2f0e2ee30052849a2149
ivasibi <ivansibilla@outlook.com>
2025-11-01 18:04:27 +0100

    bump fedora cloud to 43

--- proxmox.tf
+++ proxmox.tf
@@ -2,9 +2,9 @@ resource "proxmox_virtual_environment_download_file" "fedora" {
   node_name          = var.pve_node
   datastore_id       = var.pve_import
   content_type       = "import"
-  url                = "https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2"
-  file_name          = "Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2"
-  checksum           = "e401a4db2e5e04d1967b6729774faa96da629bcf3ba90b67d8d9cce9906bec0f"
+  url                = "https://download.fedoraproject.org/pub/fedora/linux/releases/43/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-43-1.6.x86_64.qcow2"
+  file_name          = "Fedora-Cloud-Base-Generic-43-1.6.x86_64.qcow2"
+  checksum           = "846574c8a97cd2d8dc1f231062d73107cc85cbbbda56335e264a46e3a6c8ab2f"
   checksum_algorithm = "sha256"
   upload_timeout     = var.pve_timeout
 }
