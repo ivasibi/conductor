ebf4371d78ba1f2f5fbace5266495e2983be7700
ivasibi <ivansibilla@outlook.com>
2025-11-29 10:16:31 +0100

    restructure project folders

--- modules/ascent/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "pve1_template" {
-  description = "Proxmox VE (pve1) Template"
-  type = number
-}
--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -24,9 +24,3 @@ resource "proxmox_virtual_environment_vm" "pve1_template" {
     size         = 8
   }
 }
-
-module "ascent" {
-  source = "./modules/ascent"
-
-  pve1_template = proxmox_virtual_environment_vm.pve1_template.id
-}
